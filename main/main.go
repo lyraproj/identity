04825bdd152ad7e2bfd955d511e16d835907a8aa
Scott Walker <scott.walker@puppet.com>
2019-04-04 17:52:42 +0100

    Configure logging

--- main/main.go
+++ main/main.go
@@ -1,6 +1,22 @@
 package main
 
-import "github.com/lyraproj/identity/identity"
+import (
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/lyraproj/identity/identity"
+)
+
+func init() {
+	// Configuring hclog like this allows Lyra to handle log levels automatically
+	hclog.DefaultOptions = &hclog.LoggerOptions{
+		Name:            "Identity",
+		Level:           hclog.LevelFromString(os.Getenv("LYRA_LOG_LEVEL")),
+		JSONFormat:      true,
+		IncludeLocation: false,
+		Output:          os.Stderr,
+	}
+}
 
 func main() {
 	identity.Start("identity.db")
